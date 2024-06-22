6e0f3a19c3469f2f5d529704bcea3b7b1e0ed08f
lizozom <lizka.k@gmail.com>
2024-06-22 21:15:05 +0300

    12 items

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import StreamLlmData from './components/StreamLlmData';
 const HomePage = () => {
   return (
     <div className="p-4">
-      <StreamLlmData targetNumber={20} />
+      <StreamLlmData targetNumber={12} />
     </div>
   );
 };
